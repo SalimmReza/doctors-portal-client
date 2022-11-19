3fd82c5a66e3e52d12839030aeb8c4a9250273aa
SalimmReza <salimnuaa@gmail.com>
2022-11-19 18:40:04 +0600

    make admin api called

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
@@ -129,6 +129,36 @@ async function run() {
             const user = req.body;
             console.log(user);
             const result = await usersCollections.insertOne(user);
+            res.send(result);
+        })
+
+        //find admin
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollections.findOne(query);
+            res.send({ isAdmin: user?.role === "admin" });
+        })
+
+        //make admin
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollections.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden' })
+            }
+
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollections.updateOne(filter, updateDoc, options);
             res.send(result);
         })
     }
